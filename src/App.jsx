c94ada3f9181fda3ea7e36205af69783a7cfcefc
Treasure Ajefu <treasureajefu@gmail.com>
2023-06-02 20:55:35 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+import Main from './pages/Main'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div className=''>
+        <h1 className="font-athletics text-3xl">Your trusted 
+source for bebidos muy
+deliciosos.</h1>
+        <hi className="font-ppformula font-light text-3xl">DELICIOUS & HEALTHY</hi>
+    </div>
+  )
+}
+
+export default Main
