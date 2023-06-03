cf9850ad07acc09412f9cc3dec8fbc2b94f08c6e
Treasure Ajefu <treasureajefu@gmail.com>
2023-06-03 20:27:08 +0100

    moving dino animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import ukflag from '../assets/images/uk-flag.png'
 function Navbar() {
   return (
-    <header className='px-[5%] py-4 border-b-2 border-black font-athletics font-medium text-black'>
+    <header className='px-[5%] h-[10vh] grid border-b-2 border-black font-athletics font-medium text-black'>
       <div className='grid grid-cols-3 place-items-center'>
         <div className='space-x-5 place-self-start my-auto'>
           <span>about us</span>
           <span>faqs</span>
         </div>
         <h1 className='col-span-1 font-extrabold text-[#1D5333]'>JUICE LABS</h1>
-        <div className='col-span-1 place-self-end flex items-center space-x-5'>
-          <span>contact us</span>
-          <div className='flex items-center space-x-1'>
+        <div className='col-span-1 justify-self-end flex items-center space-x-5'>
+          <span className='hidden sm:block'>contact us</span>
+          <div className='hidden sm:flex items-center space-x-1'>
               <img className='w-7' src={ukflag} alt="" />
             <span>en</span>
             <svg className='w-3' width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" clip-rule="evenodd" d="M7.39819 7.66292C7.17851 7.88259 6.82241 7.88259 6.60274 7.66292L0.867876 1.92804C0.648208 1.70837 0.648208 1.35227 0.867876 1.13259L1.13305 0.867393C1.35271 0.647718 1.70887 0.647718 1.92854 0.867393L7.00046 5.93934L12.0724 0.867393C12.2921 0.647718 12.6482 0.647718 12.8679 0.867393L13.1331 1.13259C13.3527 1.35227 13.3527 1.70837 13.1331 1.92804L7.39819 7.66292Z" fill="black"/>
             </svg>
           </div>
-          <button>shop now</button>
+          <button className='text-white bg-[#25513A] px-4 py-2 rounded-3xl border-2 border-black border-b-black border-b-4 hover:border-b-2 transition-all'>shop now</button>
         </div>
       </div>
     </header>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import heroImg from "../assets/images/hero-img.png";
+import smiley from "../assets/images/smiley.png";
+import dino from "../assets/images/dino.png";
+import "../App.css"
 function Main() {
   return (
-    <div className=''>
-        <h1 className="font-athletics text-3xl">Your trusted 
-source for bebidos muy
-deliciosos.</h1>
-        <hi className="font-ppformula font-light text-3xl">DELICIOUS & HEALTHY</hi>
+    <div className="">
+      {/* hero */}
+      <section>
+        <h1 className="px-[5%] sm:hidden text-center font-ppformula my-10 text-8xl text-[#EF4200]">DELICIOUS <br /> & <br /> HEALTHY</h1>
+        <hi className="hidden sm:flex px-[5%] text-center leading-relaxed text-[#EF4200] font-ppformula sm:text-[10.46vw]">DELICIOUS & HEALTHY</hi>
+        <div className="relative">
+          <img className="dino absolute w-24 -top-[5.4rem]" src={dino} alt="" />
+          <img className="h-80 object-cover sm:h-auto" src={heroImg} alt="" />
+          <img className="absolute right-14 top-14 w-5 sm:w-16" src={smiley} alt="" />
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
